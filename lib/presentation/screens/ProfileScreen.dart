32fd32a75d41ee1b5bec1351a55c0f8bbb470985
livmas <seiranyan.sg@gmail.com>
2024-10-07 14:27:37 +0300

    ProfileScreen.dart implemented with mock text;

--- lib/presentation/screens/ProfileScreen.dart
+++ lib/presentation/screens/ProfileScreen.dart
@@ -10,6 +10,12 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        Text("Сейранян Самвел Гагикович"),
+        Text("ЭФБО-01-22"),
+        Text("ПКС 5 задание"),
+      ],
+    );
   }
 }
